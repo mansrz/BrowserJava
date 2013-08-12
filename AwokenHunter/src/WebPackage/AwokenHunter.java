a60e38f46cdbdfbeb4184e186ae62538e67e4aeb
Jake <wolf.jake.os@gmail.com>
2013-08-11 20:33:26 -0500

    Browser1

---
+++ AwokenHunter/src/WebPackage/AwokenHunter.java
@@ -0,0 +1,14 @@
+package WebPackage;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jake
+ */
+public class AwokenHunter {
+    
+}
